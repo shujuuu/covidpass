c936923f417dde83e5021efd18ed2ab4a74ad095
shujulin <43021463+shujuuu@users.noreply.github.com>
2022-04-25 19:37:25 -0400

    Remove a paragraph for Code Review Audit

--- pages/imprint.js
+++ pages/imprint.js
@@ -32,7 +32,6 @@ export default function Imprint() {
           <p>
             Our offer contains links to external websites of third parties, on whose contents we have no influence. Therefore, we cannot assume any liability for these external contents. The respective provider or operator of the sites is always responsible for the content of the linked sites. The linked pages were checked for possible legal violations at the time of linking. Illegal contents were not recognizable at the time of linking. However, a permanent control of the contents of the linked pages is not reasonable without concrete evidence of a violation of the law. If we become aware of any infringements, we will remove such links immediately.
           </p>
-          <p className="font-bold">Credits</p>
           <p>
             With excerpts from: https://www.e-recht24.de/impressum-generator.html
             Translated with www.DeepL.com/Translator (free version)
